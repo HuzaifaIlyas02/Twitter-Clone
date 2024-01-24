5a64bc009e06fbe1255ace38ccdc9973d43b27b7
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-24 23:42:58 +0500

    Add Sidebar in components

---
+++ twitter-clone/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Sidebar.css'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      {/* Twitter icon */}
+
+      {/* SidebarOption */}
+      
+      {/* Twitter icon */}
+      
+      {/* Twitter icon */}
+
+      {/* Twitter icon */}
+      
+      {/* Button -> Tweet */}
+    </div>
+  )
+}
