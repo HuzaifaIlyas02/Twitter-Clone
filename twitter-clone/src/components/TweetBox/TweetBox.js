1c7ef95cd24863f6a2f6dc1aaf8387af26cbd11c
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-26 23:41:37 +0500

    Complete TweetBox feature

--- twitter-clone/src/components/TweetBox/TweetBox.js
+++ twitter-clone/src/components/TweetBox/TweetBox.js
@@ -4,13 +4,16 @@ import { Avatar, Button } from '@material-ui/core'
 
 function TweetBox() {
   return (
-    <div className='tweetbox'>
+    <div className='tweetBox'>
       <form>
         <div className='tweetBox__input'>
         <Avatar src="C:\Users\PMYLS\Desktop\Twitter-Clone\pngegg.png" />
         <input placeholder="What's happening?" type='text' />
         </div>
-        <Button>Tweet</Button>
+        <input className='tweetBox__imageInput'
+          placeholder='Enter image URL'
+          type='text'/>
+        <Button className='tweetBox__tweetButton'>Tweet</Button>
       </form>
     </div>
   )
