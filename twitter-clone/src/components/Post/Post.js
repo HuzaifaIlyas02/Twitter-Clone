8841d014f795784cffc035c180b75005c106c955
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-27 12:57:23 +0500

    Make a post component using with material ui

--- twitter-clone/src/components/Post/Post.js
+++ twitter-clone/src/components/Post/Post.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Post.css'
 import { Avatar } from '@material-ui/core'
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
+import RepeatIcon from '@material-ui/icons/Repeat'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import PublishIcon from '@material-ui/icons/Publish'
 
 function Post({
         // Props
@@ -15,6 +20,30 @@ function Post({
         <div className='post'>
             <div className='post__avatar'>
                 <Avatar src="C:\Users\PMYLS\Desktop\Twitter-Clone\pngegg.png" />
+            </div>
+
+            <div className='post__body'>
+                <div className='post__header'>
+                    <div className='post__headerText'>
+                        <h3>
+                            Muhammad Huzaifa Ilyas {' '}
+                            <span>
+                            <VerifiedUserIcon className='post__badge' /> @huzaifa_ilyas
+                            </span>
+                        </h3>
+                    </div>
+                    <div className='post__headerDescription'>
+                        <p>I challenge you to build a Twitter Clone with React!</p>
+                    </div>
+                </div>
+                <img src="C:\Users\PMYLS\Desktop\Twitter-Clone\pngegg.png" alt=""/>
+
+                <div className='post__footer'>
+                    <ChatBubbleOutlineIcon fontSize='small' />
+                    <RepeatIcon fontSize='small' />
+                    <FavoriteBorderIcon fontSize='small' />
+                    <PublishIcon fontSize='small' />
+                </div>
             </div>
         </div>
     )
